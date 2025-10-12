0558d9e98caf0a62f6fcb9fb65cc7a3d803bfd83
Shiva Heydari <shivaheydari.es@gmail.com>
2025-10-12 09:11:43 -0600

    us

--- src/Prototype/app.py
+++ src/Prototype/app.py
@@ -1,3 +1,11 @@
 import os
 import openai
 from dotenv import find_dotenv, load_dotenv
+from langchain_community.llms import OpenAI
+
+load_dotenv(find_dotenv())
+openai.api_key = os.getenv("Key")
+
+llm_model = "gpt-3.5-turbo"
+llm = OpenAI(openai_api_key = os.getenv("Key"))
+
