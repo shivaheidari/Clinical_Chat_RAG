a3e434debe7ea5a4ad5eb924970d45b93818c3ab
Shiva Heydari <shivaheydari.es@gmail.com>
2025-10-13 10:06:04 -0600

    phase1-3

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,12 +1,26 @@
 import pandas as pd
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_community.document_loaders import CSVLoader
 
-file_path = "/home/shiva/GIT_C/Clinical_Chat_RAG/data/NOTEEVENTS.csv"
+loader = CSVLoader(
 
+    file_path = "data/NOTEEVENTS_s.csv", 
+    content_columns="TEXT"
+)
 
-data = pd.read_csv(file_path)
+documents = loader.load()
 
-df = pd.DataFrame(data)
+text_spliter = RecursiveCharacterTextSplitter(
 
-df_100 = df.head(100)
+    chunk_size=100, 
+    chunk_overlap=20
+)
 
-df_100.to_csv("NOTEEVENTS_s.csv", index=False)
+split_documents = text_spliter.split_documents(documents)
+
+print(f"Total original documents: {len(documents)}")
+print(f"Total chunks created: {len(split_documents)}")
+print("\n--- First Chunk (ID 1) ---")
+print(f"Content: {split_documents[0].page_content}")
+print(f"Metadata: {split_documents[0].metadata}") 
