d0b8edf5bce8f5a6b886068e07af84c404bec4aa
Shiva Heydari <shivaheydari.es@gmail.com>
2025-11-27 15:40:44 -0700

    searchable

--- scripts/setup_index.py
+++ scripts/setup_index.py
@@ -13,3 +13,75 @@ VectorSearchProfile
 )
 
 import os
+import json
+
+with open("confings/azure_keys.json") as f:
+    keys = json.load(f)
+
+SEARCH_ENDPOINT = keys["SEARCH_ENDPOINT"]
+SEARCH_ADMIN_KEY = keys["SEARCH_ADMIN_KEY"]
+INDEX_NAME = "mimic-rag-index"
+
+#creating index client
+index_client = SearchIndexClient(endpoint=SEARCH_ENDPOINT, 
+                                 credential=AzureKeyCredential(SEARCH_ADMIN_KEY))
+
+#configure vector search (HNSW)
+
+vector_search = VectorSearch(
+
+    algorithms=[HnswAlgorithmConfiguration(
+
+        name="cl-hnsw",
+        parameters=HnswParameters(
+            m=4,
+            ef_construction=100,
+            ef_search=40,
+            metric="cosine"
+        )
+    )] , profiles=[VectorSearchProfile(name="hnsw-profile", algorithm_configuration_name="clinicalhnsw")]
+)
+
+# define fields (shcema)
+fileds = [
+
+
+    SimpleField(
+        name="chunk_id",
+        type=SearchFieldDataType.String,
+        key=True,
+        filterable=True
+    ),
+    SimpleField(
+        name="patinet_id",
+        type=SearchFieldDataType.String,
+        filterable=True,
+        facetable=True
+
+    ),
+    SimpleField(
+
+        name="section",
+        type=SearchFieldDataType.String,
+        filterable=True, 
+        facetable=True
+    ),
+    SearchField(
+
+        name="embedding_vector",
+        type=SearchFieldDataType.Collection(SearchFieldDataType.Single),
+        searchable=True,
+        vector_search_dimensions=1536,
+        vector_search_profile_name="hnsw-profile"
+    )
+]
+
+index = SearchIndex(
+
+    name=INDEX_NAME,
+    fields=fileds,
+    vector_search=vector_search
+)
+
+result = index_client.create_or_update_index(index)
+print(f"index '{result.name}' created/updated")
